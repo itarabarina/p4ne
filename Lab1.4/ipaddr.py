374546342ad550c68e68ead9bf9a2aa28e95bda3
Irina Tarabarina <itarabarina@jet.msk.su>
2019-03-14 18:00:15 +0300

    Лабораторная работа 1.2

--- Lab1.4/ipaddr.py
+++ Lab1.4/ipaddr.py
@@ -7,17 +7,18 @@ class IPv4RandomNetwork(IPv4Network):
     def __init__(self, start_1=0, stop_1=100):
         IPv4Network.__init__(self, (random.randint(0x0B000000, 0xDF000000), random.randint(start_1, stop_1), False), strict = False)
 
-
-
     def regular(self):
-        if self.is_global: return True
-        else: return False
+        if self.is_global: return 1
+        elif self.is_multicast or self.is_unspecified or self.is_private or self.is_loopback or self.is_link_local: return 2
+
+    def key_value(self):
+        return int(IPv4Network.network_address) + int(IPv4Network.netmask)
 
 list_ip = []
 
 for i in range(0, 50):
     network1=IPv4RandomNetwork(8, 24)
-    list_ip.append(network1)
+    if network1.regular(): list_ip.append(network1)
 
 for i in list_ip:
     print(i)
