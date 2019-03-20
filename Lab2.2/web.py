a62b32015c134c686f5743adb5ddc9dcea368b35
Irina Tarabarina <itarabarina@jet.msk.su>
2019-03-20 11:00:32 +0300

    Лабораторная работа 2.2

--- Lab2.2/web.py
+++ Lab2.2/web.py
@@ -3,7 +3,6 @@
 from flask import Flask
 import glob
 import re
-import json
 import pprint
 
 hosts={}
@@ -13,28 +12,28 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    help="Для получения списка имен выполните закпро к /configs. Для получения списка ip адресов нга хосте выполните запрос на /config/hostname"
+    help="Для получения списка имен хостов выполните запрос к /configs. Для получения списка ip адресов хоста выполните запрос на /config/<hostname> где hostname - имя выбранного хоста"
     return help
 
 @app.route('/configs')
 def list_hostname():
    name=[]
-   for h in hosts.keys():
-       name.append(hosts[h]['hostname'])
+   for i in hosts.keys():
+       name.append(hosts[i]['hostname'])
    return str(name)
 
 
 @app.route('/config/<hostname>')
 def list_ip(hostname):
-    for h in hosts.keys():
-        if hosts[h]['hostname'] == hostname:
-            return str(hosts[h]['ip'])
+    for j in hosts.keys():
+        if hosts[j]['hostname'] == hostname:
+            return str(hosts[j]['ip'])
         else: return "ERROR"
             
 if __name__ == '__main__':
 
     for current_file in glob.glob("/home/irina/Projects/python/Seafile/p4ne_training/config_files/*.txt"):
-        m=re.match("/home/irina/Projects/python/Seafile/p4ne_training/config_files/(.*)_([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*", current_file)
+        m=re.match("^.*/config_files/(.*)_([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*", current_file)
         if m: s=m.group(1)
         #print(current_file_name)
         hosts[s] = {}
@@ -50,7 +49,7 @@ if __name__ == '__main__':
                 if i:
                     hosts[s]['ip'].append(i.group(1))
 
-#pprint.pprint(hosts)
+pprint.pprint(hosts)
 
 app.run(debug=True)
   
